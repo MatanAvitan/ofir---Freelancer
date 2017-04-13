18e6a2eabb21fde831d4ae8e9e08d809142fa927
ma <matan13av@gmail.com>
2017-04-13 18:23:09 +0300

    angular

--- js/angularjs/app.js
+++ js/angularjs/app.js
@@ -2,7 +2,7 @@
  * Created by Matan on 4/13/2017.
  */
 
-var app = angular.modules('FreeLancer', ['ngRoute','ngResource']);
+var app = angular.module('FreeLancer', ['ngRoute','ngResource']);
 
 app.config(['$routeProvider', function ($provider) {
     $routeProvider
