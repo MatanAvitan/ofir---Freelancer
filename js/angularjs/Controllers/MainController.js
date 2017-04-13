a8f316af715c9fc16e62d53698cc04a361826693
ma <matan13av@gmail.com>
2017-04-13 18:25:58 +0300

    angular

--- js/angularjs/Controllers/MainController.js
+++ js/angularjs/Controllers/MainController.js
@@ -2,6 +2,6 @@
  * Created by Matan on 4/13/2017.
  */
 
-FreeLancer.controller('MainController', ['$scope', function($scope) {
+FreeLancer.controller('FreeLancer.MainController', ['$scope', function($scope) {
     alert('sdgsgs');
 }]);
