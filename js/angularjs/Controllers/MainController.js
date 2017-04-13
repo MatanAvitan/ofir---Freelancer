9b3c51db5bd01c79865279a1d8fad3b7dfba754c
ma <matan13av@gmail.com>
2017-04-13 18:47:16 +0300

    AngularSheet

--- js/angularjs/Controllers/MainController.js
+++ js/angularjs/Controllers/MainController.js
@@ -2,20 +2,21 @@
  * Created by Matan on 4/13/2017.
  */
 
-app.controller('FreeLancer.MainController', ['$scope', function($scope) {
+app.controller('FreeLancer.MainController', ['$scope', function ($scope) {
     // create a blank object to handle form data.
     $scope.user = {};
     // calling our submit function.
-    $scope.submitForm = function() {
+    $scope.submitForm = function () {
         // Posting data to php file
         $http({
-            method  : 'POST',
-            url     : 'https://vshma.github.io/ofir---Freelancer/data',
-            data    : $scope.user, //forms user object
-            headers : {'Content-Type': 'application/x-www-form-urlencoded'}
+            method: 'POST',
+            url: 'https://vshma.github.io/ofir---Freelancer/data',
+            data: $scope.user, //forms user object
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
         })
-            .success(function(data) {
-                    $scope.message = data.message;
+            .success(function (data) {
+                $scope.message = data.message;
+                console.log($scope.message);
             });
     };
 }]);
