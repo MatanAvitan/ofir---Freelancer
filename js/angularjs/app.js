039786f65670900283b6e7cb932426314af83e08
ma <matan13av@gmail.com>
2017-04-16 17:06:41 +0300

    data

--- js/angularjs/app.js
+++ js/angularjs/app.js
@@ -2,11 +2,11 @@
  * Created by Matan on 4/13/2017.
  */
 
-var app = angular.module('FreeLancer', ['ngRoute','ngResource']);
+var app = angular.module('FreeLancer', ['ngRoute', 'ngResource']);
 
 app.config(['$routeProvider', function ($provider) {
     $routeProvider
-        .when('/index',{
+        .when('/',{
             controller: 'FreeLancer.MainController',
             templateUrl: 'Ofir - Freelancer/index.html'
         });
