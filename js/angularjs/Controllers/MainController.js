fc0368026ee8e88bf3351acad5c31feb9e5fedbc
ma <matan13av@gmail.com>
2017-04-19 20:02:29 +0300

    With Data

--- js/angularjs/Controllers/MainController.js
+++ js/angularjs/Controllers/MainController.js
@@ -7,12 +7,12 @@ app.controller('FreeLancer.MainController', ['$scope', function ($scope) {
     // create a blank object to handle form data.
     $scope.user = {};
     // calling our submit function.
-    $scope.submitForm = function () {
+    $scope.submitForm = function ($scope, $http) {
         console.log('Submited');
         // Posting data to php file
         $http({
             method: 'POST',
-            url: '127.0.0.1:5000/data',
+            url: '/data',
             data: $scope.user, //forms user object
             headers: {'Content-Type': 'application/x-www-form-urlencoded'}
         })
