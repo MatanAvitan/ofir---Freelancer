8191556ebaaceebb05dd973a89c16fd9da248efa
ma <matan13av@gmail.com>
2017-04-19 20:12:02 +0300

    real

--- js/angularjs/Controllers/MainController.js
+++ js/angularjs/Controllers/MainController.js
@@ -5,7 +5,7 @@
 app.controller('FreeLancer.MainController', ['$scope', '$http', function ($scope) {
     console.log('Entered');
     // create a blank object to handle form data.
-    $scope.user = {"None"};
+    $scope.user = {"None":"None"};
     // calling our submit function.
     $scope.submitForm = function ($scope, $http) {
         console.log('Submited');
