3b0dac4085389118852fd25afc4840db5d2ec5f6
ma <matan13av@gmail.com>
2017-04-22 20:50:22 +0300

    Print user in scope

--- js/angularjs/Controllers/MainController.js
+++ js/angularjs/Controllers/MainController.js
@@ -6,6 +6,7 @@ app.controller('FreeLancer.MainController', ['$scope', '$http', function ($scope
     console.log('Entered');
     // create a blank object to handle form data.
     $scope.user = {};
+    console.log($scope.user);
     // calling our submit function.
     $scope.submitForm = function ($scope, $http) {
         console.log('Submited');
